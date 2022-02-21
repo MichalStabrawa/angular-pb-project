702d344bd8f2d8bd710445124e291431339f5330
MichalStabrawa <mistaaqua@gmail.com>
2022-02-21 23:10:09 +0100

    ngModule add to form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
+import { AppComponent } from "./app.component";
+import { FormComponent } from "./form/form.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, FormComponent],
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -6,23 +6,15 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./form.component.css"],
 })
 export class FormComponent implements OnInit {
-    public name: string = "";
-    public email: string = "";
-    public isDisabled: boolean = false;
-    public isVisible: boolean = false;
+    name: string = "";
+    email: string = "";
+    isDisabled: boolean = false;
+    isVisible: boolean = false;
+    test = "";
     show() {
         alert("Dziala");
     }
-    onInputChange(event: any): void {
-        if (event.target.type === "text") {
-            console.log(event.target.type);
-            this.name = event.target.value;
-        } else if (event.target.type === "email") {
-            console.log(event.target.type);
-        } else {
-            null;
-        }
-    }
+
     constructor() {}
 
     ngOnInit(): void {}
