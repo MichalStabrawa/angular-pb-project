5b9c402f7510aaac85737d9465f7515639b10526
MichalStabrawa <mistaaqua@gmail.com>
2022-02-23 21:10:30 +0100

    add game over

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewChild } from "@angular/core";
 import { User } from "../form/form.component";
+import { TetrisCoreComponent } from "ngx-tetris";
 
 @Component({
     selector: "app-game",
@@ -8,8 +9,69 @@ import { User } from "../form/form.component";
 })
 export class GameComponent implements OnInit {
     @Input() user!: User; // decorate the property with @Input()
+    @ViewChild(TetrisCoreComponent)
+    private _tetris!: TetrisCoreComponent;
+
+    public onStart() {
+        this._tetris.actionStart();
+        this.timerGo();
+        this.onPlayAfterpause();
+    }
+
+    public onPauseSet() {
+        this._tetris.actionStop();
+        this.onPause();
+    }
+
+    public onReset() {
+        this._tetris.actionReset();
+        this.onResetStatus();
+    }
+
+    public counter: number = 0;
+    public timerHours: number = 0;
+    public timerMinutes: number = 0;
+    public timerSeconds: number = 0;
+    public status: string = "Ready to game:";
+    public endGame: boolean = false;
 
     constructor() {}
 
     ngOnInit(): void {}
+
+    public onGrow() {
+        this.counter += 1;
+    }
+    public timerGo() {
+        setInterval(() => {
+            if (this.timerSeconds < 59) {
+                this.timerSeconds++;
+            } else {
+                this.timerSeconds = 0;
+                this.timerMinutes++;
+                if (this.timerMinutes == 59) {
+                    this.timerMinutes = 0;
+                    this.timerHours++;
+                }
+            }
+        }, 1000);
+    }
+
+    public onPause() {
+        this.status = "Paused";
+    }
+
+    public onPlayAfterpause() {
+        this.status = "Playing";
+    }
+
+    public onResetStatus() {
+        this.status = "Ready to game";
+        this.counter = 0;
+    }
+
+    public gameOver() {
+        alert("Game Over");
+        this.endGame = true;
+    }
 }
